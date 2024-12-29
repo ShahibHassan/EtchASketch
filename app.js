07a3b3d84db54720413a4592a2585de707327599
ShahibHassan <67552203+ShahibHassan@users.noreply.github.com>
2024-12-29 20:03:05 +0000

    - created a webpage which displays a 16 x 16 grid - Need to figure out why theres an extra row ie fix height and width of container div - set up function for hover effect.

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
-function createGrid(){
-    const gridContainer = document.querySelector('.container')
+function createGrid() {
+  const gridContainer = document.querySelector('.container');
+  console.log(gridContainer);
 
-    for (var i = (16*16); i>=1; i--){
-        var square = document.createElement('div')
-        gridContainer.appendChild(square).className = 'grid';
-        console.log(gridContainer);
-    }
+  //create a for loop to insert divs into container
+  for (var i = 0; i < 256; i++) {
+    console.log('div number: ' + (i + 1));
+    var square = document.createElement('div');
+    square.style.backgroundColor = 'white';
+    square.style.height = '40px';
+    square.style.width = '40px';
+    square.style.border = '1px solid black';
+    square.style.display = 'flex';
+    gridContainer.appendChild(square).className = 'square';
+  }
+  console.log(gridContainer);
+  onHover();
+}
+
+function onHover() {
+  const squares = document.querySelectorAll('.square');
+
+  squares.forEach((square) => {
+    square.addEventListener('mouseover', () => {
+      square.style.backgroundColor = 'red';
+    });
+  });
 }
 
 createGrid();
-
-const squares = document.querySelectorAll('.grid');
-squares.forEach((div) => {
-div.addEventListener('mouseover', () => {
-    div.style.backgroundColor = 'red';
-    console.log(div);
-})
-});
