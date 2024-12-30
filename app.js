02b7eda3e5a8f3ce205f7589900ab41b5c5188f6
Shahib Hassan <67552203+ShahibHassan@users.noreply.github.com>
2024-12-30 00:18:48 +0000

    I have now fixed the issue with the dimensions of the grid by adding by adding box sizing to the squares. I am now working on adding a function which allows us to enter the dimensions of the grid with a click of a button

--- app.js
+++ app.js
@@ -2,15 +2,17 @@ function createGrid() {
   const gridContainer = document.querySelector('.container');
   console.log(gridContainer);
 
+  const gridDimensions = getGridSize()
+
   //create a for loop to insert divs into container
-  for (var i = 0; i < 256; i++) {
+  for (var i = 0; i < gridDimensions; i++) {
     console.log('div number: ' + (i + 1));
     var square = document.createElement('div');
     square.style.backgroundColor = 'white';
     square.style.height = '40px';
     square.style.width = '40px';
     square.style.border = '1px solid black';
-    square.style.display = 'flex';
+    square.style.boxSizing = 'border-box'; // fixed issue with rows and columns != by removing the border from total size
     gridContainer.appendChild(square).className = 'square';
   }
   console.log(gridContainer);
@@ -25,6 +27,20 @@ function onHover() {
       square.style.backgroundColor = 'red';
     });
   });
+}
+
+function getGridSize() {
+  const gridSize = document.querySelector('.btn');
+  gridSize.addEventListener('onclick', () => {
+    console.log(gridSize);
+    var dimensions = promt('Please enter Dimension p to 32 (e.g. for 10 x 10, enter 10)')
+    if ((dimensions > 36) || (dimensions < 1) ) {
+      dimensions = promt('please enter a valid number (between 1-32)');
+    }
+    else {
+    return dimensions
+    }
+    })
 }
 
 createGrid();
