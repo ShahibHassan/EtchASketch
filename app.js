ef3be9a8b89c109972dc640bf22fcc32a3b9a022
Shahib Hassan <67552203+ShahibHassan@users.noreply.github.com>
2025-01-05 21:42:19 +0000

    Implemented gridSize function which allows user to click a button to enter the grid size. The grid size will then generate with a strict container width and height (640px).

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-function createGrid() {
+function createGrid(gridSize) {
   const gridContainer = document.querySelector('.container');
   console.log(gridContainer);
+  gridContainer.innerHTML = '';
 
-  const gridDimensions = getGridSize()
+  const squareSize = 640 / gridSize;
 
   //create a for loop to insert divs into container
-  for (var i = 0; i < gridDimensions; i++) {
+  for (var i = 0; i < gridSize * gridSize; i++) {
     console.log('div number: ' + (i + 1));
     var square = document.createElement('div');
     square.style.backgroundColor = 'white';
-    square.style.height = '40px';
-    square.style.width = '40px';
+    square.style.height = squareSize + 'px';
+    square.style.width = squareSize + 'px';
     square.style.border = '1px solid black';
     square.style.boxSizing = 'border-box'; // fixed issue with rows and columns != by removing the border from total size
     gridContainer.appendChild(square).className = 'square';
@@ -29,18 +30,18 @@ function onHover() {
   });
 }
 
-function getGridSize() {
-  const gridSize = document.querySelector('.btn');
-  gridSize.addEventListener('onclick', () => {
-    console.log(gridSize);
-    var dimensions = promt('Please enter Dimension p to 32 (e.g. for 10 x 10, enter 10)')
-    if ((dimensions > 36) || (dimensions < 1) ) {
-      dimensions = promt('please enter a valid number (between 1-32)');
+function getGridSize(){
+  const btn = document.querySelector('.btn');
+  btn.addEventListener('click', () => {
+    var userInput = prompt ('Please enter the number of squares per grid side (Between 1 and 100)');
+    while ((userInput < 1) || (userInput > 100)) {
+      userInput = prompt ('Please enter a valid number between 1 and 100')
     }
-    else {
-    return dimensions
-    }
-    })
+    createGrid(Number(userInput));
+  })
 }
 
-createGrid();
+
+//default gridSize is 16x16
+createGrid(16);
+getGridSize();
